0c895e466c06b7f33bca54a9c976063780c2f54c
imnotthomas <thomasjaltman@gmail.com>
2016-03-23 21:18:13 +0000

    recreating as node app

---
+++ node-express-angular/public/app/app.module.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  angular
+    .module("app", [
+      // Angular modules
+      'ngRoute',
+
+      // Third party modules
+      'firebase',
+
+      // Custome modules
+      'app.landing'
+    ]);
+
+})();
