034505ffe7b4c6e49ec1f15c92af4b5428925df4
imnotthomas <thomasjaltman@gmail.com>
2016-03-24 23:27:37 +0000

    setting up firebase

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -3,15 +3,18 @@
     .module('app.waitList')
     .controller('waitListController', waitListController);
 
-  function waitListController() {
-    var vm = this;
+  waitListController.$inject = ['$firebaseArray'];
 
-    vm.parties = [1, 2, 3, 4];
+  function waitListController($firebaseArray) {
+    var vm = this;
+    var fireParties = new Firebase('https://waitandeat-thomas.firebaseio.com/parties');
+
+    vm.parties = $firebaseArray(fireParties);
 
     vm.addParty = addParty;
 
     function addParty() {
-      vm.parties.push('another');
+      vm.parties.$add('another');
     }
   }
 })();
