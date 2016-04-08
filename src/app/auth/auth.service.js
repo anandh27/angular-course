1674af8124445da5cb1322593e639f722cee9b9a
imnotthomas <thomasjaltman@gmail.com>
2016-04-08 12:52:43 +0000

    building auth service

---
+++ src/app/auth/auth.service.js
@@ -0,0 +1,23 @@
+(function() {
+  'use strict';
+
+  angular
+    .module('app.auth')
+    .factory('authService', authService);
+
+  authService.$inject = ['$firebaseAuth', 'firebaseDataService'];
+
+  function authService($firebaseAuth, firebaseDataService) {
+    var firebaseAuthObject = $firebaseAuth(firebaseDataService.root);
+    var service = {
+      register: register
+    };
+
+    return service;
+
+    ///////////////
+    function register(user) {
+      return firebaseAuthObject.$createUser(user);
+    }
+  }
+});
