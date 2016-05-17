784e64db65710c5c75640c97625c88a007ced1cc
imnotthomas <thomasjaltman@gmail.com>
2016-05-17 23:03:02 +0000

    adding email notification

--- src/app/auth/auth.service.js
+++ src/app/auth/auth.service.js
@@ -14,7 +14,8 @@
       register: register,
       login: login,
       logout: logout,
-      isLoggedIn: isLoggedIn
+      isLoggedIn: isLoggedIn,
+      sendWelcomeEmail: sendWelcomeEmail
     };
 
     return service;
@@ -34,6 +35,11 @@
 
     function isLoggedIn() {
       return firebaseAuthObject.$getAuth();
+    }
+
+    function sendWelcomeEmail(emailAddress) {
+      firebaseDataService.emails.push({
+        emailAddress: emailAddress});
     }
   }
 })();
